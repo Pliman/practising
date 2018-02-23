cc36de2edaf17fe11ddbcdc4e5f9497a79cd8dbf
孙彬 <bin.sun@dianrong.com>
2018-02-23 17:46:54 +0800

    self binary search.

--- algorithm/search/binary-search.js
+++ algorithm/search/binary-search.js
@@ -1,18 +1,18 @@
-var arr = [1,2,3,4,5,6,7,8,9,10,13,24,35,46,47,78,123];
+var arr = [1,2,3,4,5,6,7,8,9,10,13,24,35,46,47,78,123]; // 17个
 
 function binarySearch (arr, target) {
   var indexes = arr.length - 1;
 
   if (indexes === 0) {
-    return target === arr[0] ? arr[0] : -1;
+    return target === arr[0] ? 0 : -1;
   }
 
   var point = Math.floor(indexes / 2);
 
   if (target > arr[point]) {
-    return point + 1 + binarySearch(arr.slice(point + 1));
+    return point + 1 + binarySearch(arr.slice(point + 1), target);
   } else if (target < arr[point]) {
-    return binarySearch(arr.slice(0, point - 1));
+    return binarySearch(arr.slice(0, point), target);
   } else {
     return point;
   }
@@ -20,4 +20,4 @@ function binarySearch (arr, target) {
   return "shouldn't reach here";
 }
 
-console.log(binarySearch(arr, 46));
+console.log(binarySearch(arr, 123));
