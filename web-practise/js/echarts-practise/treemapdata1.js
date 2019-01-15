3e368ff5b27021754b0ca79dd90ea2c5af7dc3a3
pliman <sunbin1@beisen.com>
2019-01-15 10:51:07 +0800

    echarts practise

---
+++ web-practise/js/echarts-practise/treemapdata1.js
@@ -0,0 +1,34 @@
+var data = [{
+    "name": "xAxis",
+    "children": [
+        {name: 'a', value: 5364},
+        {name: 'b', value: 3436},
+        {name: 'c', value: 2928},
+        {name: 'd', value: 2677},
+        {name: 'e', value: 2313},
+        {name: 'f', value: 2212},
+        {name: 'g', value: 1889},
+        {name: 'h', value: 1667},
+        {name: 'i', value: 1241},
+        {name: 'j', value: 986},
+        {name: 'k', value: 777},
+        {name: 'l', value: 543},
+        {name: 'm', value: 389}
+    ]
+}]
+
+data = [
+    {name: 'a', value: 5364},
+    {name: 'b', value: 3436},
+    {name: 'c', value: 2928},
+    {name: 'd', value: 2677},
+    {name: 'e', value: 2313},
+    {name: 'f', value: 2212},
+    {name: 'g', value: 1889},
+    {name: 'h', value: 1667},
+    {name: 'i', value: 1241},
+    {name: 'j', value: 986},
+    {name: 'k', value: 777},
+    {name: 'l', value: 543},
+    {name: 'm', value: 389}
+]
