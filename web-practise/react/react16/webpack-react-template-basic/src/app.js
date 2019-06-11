67e8216345431541efad28b27dfb193f43ec6814
sunbin1 <sunbin1@beisen.com>
2019-06-11 19:01:18 +0800

    渲染结果

--- web-practise/react/react16/webpack-react-template-basic/src/app.js
+++ web-practise/react/react16/webpack-react-template-basic/src/app.js
@@ -2,14 +2,56 @@ import React from 'react'
 import ReactDOM from "react-dom"
 
 class App extends React.Component {
+    constructor(args) {
+        super(args)
+        this.state = {
+            items: this.genItems()
+        }
+    }
+
+    genItems() {
+        console.time('gen items')
+
+        let items = []
+
+        for (let i = 0; i < 10000; i++) {
+            items.push({
+                name: `name${i}`,
+                age: i
+            })
+        }
+
+        console.timeEnd('gen items')
+
+        return items
+    }
+
+    componentWillMount() {
+        console.log('m1')
+        console.time('mount')
+    }
+
+    componentDidMount() {
+        console.timeEnd('mount')
+    }
+
     render() {
-        return (
-            <div>123</div>
-        )
+        console.log('r2')
+        console.time('render items')
+
+        let renderedItems = this.state.items && this.state.items.length > 0
+            && this.state.items.map(item =>
+                <div key={item.age}><label>{item.name}</label><input defaultValue={item.age}/></div>
+            );
+
+        console.timeEnd('render items')
+
+        return renderedItems
+
     }
 }
 
 ReactDOM.render(
-    <App />,
+    <App/>,
     document.getElementById("app")
 )
