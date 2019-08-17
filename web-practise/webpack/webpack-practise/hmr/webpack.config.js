15e2e6fc5bf8a2c295e9a72c8a4f775cd289e806
sunbin1 <sunbin1@beisen.com>
2019-08-17 09:49:23 +0800

    webpack hmr

---
+++ web-practise/webpack/webpack-practise/hmr/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        app: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    plugins: [
+        new CleanWebpackPlugin.CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: '模块热替换'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
