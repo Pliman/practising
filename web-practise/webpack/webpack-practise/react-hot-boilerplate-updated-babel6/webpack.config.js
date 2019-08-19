dc5ba45ccb2be557baa79dcbc0cb9a8d64b26fc4
sunbin1 <sunbin1@beisen.com>
2019-08-19 23:31:05 +0800

    react hot loader old and updated.

---
+++ web-practise/webpack/webpack-practise/react-hot-boilerplate-updated-babel6/webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        'react-hot-loader/patch',
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: ['babel-loader'],
+            exclude: /node_modules/
+        }]
+    },
+    mode: 'development'
+};
