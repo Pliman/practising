6591b1ea98b2642dd6912c222676857f960057b2
sunbin1 <sunbin1@beisen.com>
2019-08-20 23:24:20 +0800

    remote react hot loader in entry. works.

--- web-practise/webpack/webpack-practise/react-hot-boilerplate-updated-dev-server/webpack.config.js
+++ web-practise/webpack/webpack-practise/react-hot-boilerplate-updated-dev-server/webpack.config.js
@@ -4,7 +4,6 @@ var webpack = require('webpack');
 module.exports = {
     devtool: 'cheap-module-eval-source-map',
     entry: [
-        'react-hot-loader/patch',
         './src/index'
     ],
     output: {
