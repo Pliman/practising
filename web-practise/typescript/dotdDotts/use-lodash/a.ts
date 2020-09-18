8923047881c4dea89d0263a30764614449167fc6
sunbin1 <sunbin1@beisen.com>
2020-09-18 15:13:11 +0800

    use @types

--- web-practise/typescript/dotdDotts/use-lodash/a.ts
+++ web-practise/typescript/dotdDotts/use-lodash/a.ts
@@ -1,3 +1,3 @@
 import * as lodash from 'lodash'
 
-lodash.fake()
+console.log(lodash.filter([ 1, 2, 3, 4, 5, 6 ], (x: number) => x > 3))
--- web-practise/typescript/dotdDotts/use-lodash/modulesanyname.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'lodash' {
-    function fake(): void
-}
